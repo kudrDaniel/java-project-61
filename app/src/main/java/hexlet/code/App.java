1fca5040cd8f724d2f73a1fbd146ca22da6f2d70
kudrDaniel <kudr.daniel.vlad.ch@gmail.com>
2023-05-12 16:57:10 +0300

    Add Makefile, modify build.gradle, hexlet.code.App, .gitignore, README.md

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,13 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games");
+        System.out.print("May I have your name? ");
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+        System.out.println(String.format("Hello? %s!", userName));
     }
 }
